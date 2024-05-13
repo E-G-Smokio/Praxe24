f5da4850859118ec9927cd3e0a6ea27d81a42e6c
E-G-Smokio <pliska.matyas@gmail.com>
2024-05-13 18:11:56 +0200

    x

---
+++ testovaci2/testovaci2/apliace/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def aplikace(request):
+    return HttpResponse("Funguj!!!")
+# Create your views here.
--- testovaci2/testovaci2/wsgi.py
+++ testovaci2/testovaci2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for testovaci project.
+WSGI config for testovaci2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testovaci.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testovaci2.settings')
 
 application = get_wsgi_application()
