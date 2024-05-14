94bb935906519bd35df62ac1ee4dbe848de85d5e
E-G-Smokio <pliska.matyas@gmail.com>
2024-05-14 13:16:24 +0200

    Prvni funkci url

--- auta/views.py
+++ auta/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return render(request, "auta/hello.html")
+    return render(request, "auta/hello.html", {'name' : 'prvni stranka'})
