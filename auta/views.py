b35925b75403259372495b6fcc3da1ae0123ab0f
E-G-Smokio <pliska.matyas@gmail.com>
2024-05-15 10:32:08 +0200

    homepage, main template

--- auta/views.py
+++ auta/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+autaList = [
+    {'id' : 1, 'value' : 'Opel'},
+    {'id' : 2, 'value' : 'Astra'}, 
+    {'id' : 3, 'value' : 'G'}
+]
+
 def home(request):
-    return render(request, 'homePage.html')
+    kontekext = {'autaList' : autaList}
+    return render(request, 'homePage.html', kontekext)
 
 def say_hello(request):
     return render(request, "hello.html", {'name' : 'prvni stranka'})
